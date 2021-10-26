856e0c5b1375e0c71f025e491df42c5f09f1dcf5
samir.mirzaliyev <samir.mirzaliyev@cybernet.az>
2021-10-26 23:07:18 +0400

    first version auth was created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { auth, signInWithGoogle } from './firebase/config';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [currentUser,setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const checkUser = () => auth.onAuthStateChanged(user => setCurrentUser(user))
+    return checkUser();
+  },[])
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +27,12 @@ function App() {
         >
           Learn React
         </a>
+        {
+          currentUser ?
+          <button onClick={() => auth.signOut()}>Sign Out</button> 
+          :
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
+        }
       </header>
     </div>
   );
---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+
+const config = {
+  apiKey: "AIzaSyAPClbIIurScmtNWhhv077bIB_VL0GMMdc",
+  authDomain: "testdb-65d57.firebaseapp.com",
+  projectId: "testdb-65d57",
+  storageBucket: "testdb-65d57.appspot.com",
+  messagingSenderId: "94083680534",
+  appId: "1:94083680534:web:cd6b83a4fa64bf373aa837"
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: "select_account"});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
