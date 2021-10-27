58f7888d02038d56f0107dd7323b2ac08f3d44cb
samir.mirzaliyev <samir.mirzaliyev@cybernet.az>
2021-10-27 17:09:08 +0400

    added prettier and protected render

--- src/views/ui/Authentication/Authentication.tsx
+++ src/views/ui/Authentication/Authentication.tsx
@@ -2,22 +2,30 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import mainRoutes from '../routes';
 
-
 import Login from './pages/Login';
 import Registration from './pages/Registration';
+import { useSelector } from 'react-redux';
+import { authSelector } from '../../../services/selectors/authSelector';
 
-interface iProps {
-}
+interface iProps {}
 
-const Authentication:React.FC<iProps> = (): JSX.Element => {
+const Authentication: React.FC<iProps> = (): JSX.Element => {
+  const { currentUser } = useSelector(authSelector);
 
-    return (
-        <Switch>
-            <Route exact path={mainRoutes.Authentication.login} component={Login} />
-            <Route exact path={mainRoutes.Authentication.registration} component={Registration} />
-            <Redirect to={mainRoutes.Authentication.login} />
-        </Switch>
-    );
-}
+  return (
+    <Switch>
+      <Route
+        exact
+        path={mainRoutes.Authentication.login}
+        render={() => (currentUser ? <Redirect to="/" /> : <Login />)}
+      />
+      <Route
+        exact
+        path={mainRoutes.Authentication.registration}
+        render={() => (currentUser ? <Redirect to="/" /> : <Registration />)}
+      />
+    </Switch>
+  );
+};
 
 export default Authentication;
